2059e95b24fbd065f7a7c1ea75ce2c785c27e774
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2023-04-13 09:59:58 +0200

    Lab 1 - Include & Require (Part 3)

---
+++ content.php
@@ -0,0 +1,7 @@
+<?php
+  $myName = "Robin Vervoorn";
+
+  echo $myName . '<br>';
+  echo '<img src="ik-wil-fietsen.jpg" alt="IK WIL FIETSEN"> <br>';
+  echo 'een paragraaf aan lorem ipsum text. <br> <br>';
+?>
--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-  require "variables.php";
-  echo "<p>" . json_encode($fruits) . "</p>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    for ($index = 0; $index < 10; $index++) {
+      require "content.php";
+    }
+  ?>
+</body>
+</html>
